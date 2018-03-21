dc61965cb7e57af0b7a6f12384891433218d2199
MyRa-7 <mytibala@gmail.com>
2018-03-21 20:02:17 +0530

    Muggianu Works

--- ASM2/asm2q1.m
+++ ASM2/asm2q1.m
@@ -1,23 +1,43 @@
 % Using Wang's Formula to get Muggianu, Colinet and Kohler Approximations
 1;
 c=4; % quaternary 
-
+x=[0.25; 0.25; 0.25;0.25]; % order [Co; Cu; Fe; Ni]
 % scheme related values
 t_ij=1;
 beta_ij=1;
 lambda_ij=0;
-
+h_E=0;
 % The general schema :
 for i=1:c-1
-   for j=i+1:c
-      for k=1:t_ij
-          [a,b,c]=parameters(
-          h_E=beta_ij
-      end
-   end
+    for j=i+1:c
+        for k=1:t_ij
+            %           disp(num2str(i));
+            %           disp(num2str(j));
+            [p,q,r]=parameters(x(i),x(j),lambda_ij);
+            
+            if(i==1)
+                if(j==2)
+                    gE=fCoCu(p,q);
+                elseif(j==3)
+                    gE=fCoFe(p,q);
+                else
+                    gE=fCoNi(p,q);
+                end
+            elseif(i==2)
+                if(j==3)
+                    gE=fCuFe(p,q);
+                else
+                    gE=fCuNi(p,q);
+                end
+            else
+                gE=fFeNi(p,q);
+            end
+            h_E=h_E+beta_ij*r*gE;
+        end
+    end
 end
 
-function [x_ij,x_ji,f_ij]=parameters(x_1,x_2)
+function [x_ij,x_ji,f_ij]=parameters(x_1,x_2,lambda_ij)
 x_ij=0.5*((1+x_1-x_2)+lambda_ij*(1-x_1-x_2));
 x_ji=0.5*((1+x_2-x_1)+lambda_ij*(1-x_1-x_2));
 f_ij=(x_1*x_2)/(x_ij*x_ji);
