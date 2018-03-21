0ba55871a3163546f2214298832f74b5ca98d4ab
MyRa-7 <mytibala@gmail.com>
2018-03-21 23:49:43 +0530

    Code Beautified. Assignment 2 done.

--- ASM2/asm2q1.m
+++ ASM2/asm2q1.m
@@ -1,25 +1,12 @@
-% Using Wang's Formula to get Muggianu, Colinet and Kohler Approximations
+% Using Wang's Formula to get Muggianu, Colinet and Kohler Extrapolations
 1;
-clear;
+clc; clear; close all;
 
 c=4; % quaternary
-% x=[0.25 0.25 0.25 0.25]; % order [Co; Cu; Fe; Ni]
-% scheme related values
-                % % muggianu
-                t_ij=1;
-                beta_ij=1;
-                
-                
-                % % Kohler
-%                 t_ij=1;
-%                 beta_ij=1;
-%                
-                
-                % % Colinet
-%                 t_ij=2;
-%                 beta_ij=0.5;
+
 n=100;
 z=60/n;
+% order [Co; Cu; Fe; Ni]
 x=zeros(n,4);
 for jj=1:n
     x(jj,1)=0.1;
@@ -27,36 +14,73 @@ for jj=1:n
     x(jj,3)=(60-jj*z)/100;
     x(jj,4)=(jj*z)/100;
 end
-h_E=zeros(size(x,1),1);
 
+fprintf('Part (a)\n\n');
+h_e_muggianu=schemeswitch('m',c,x);
+h_e_kohler=schemeswitch('k',c,x);
+h_e_colinet=schemeswitch('c',c,x);
+
+figure(1);
+hold on;
+plot(x(:,4),h_e_muggianu,'g-o',"linewidth",1.5);
+plot(x(:,4),h_e_kohler,'b-+',"linewidth",1.5);
+plot(x(:,4),h_e_colinet,'r-x',"linewidth",1.5);
+title("Plot of \Delta_{mix} H_m (J-mol^{-1})","FontSize",18);
+xlabel("Mole Fraction of Ni","FontSize",14);
+ylabel("\Delta_{mix} H_m (J-mol^{-1})","FontSize",14);
+axis('square');
+set(gca,'FontSize',16);
+legend("Muggianu","Kohler","Colinet","location","NorthEast");
+
+fprintf('Part (b)\n');
+x=0.25*ones(1,c);
+h_e_muggianu=schemeswitch('m',c,x);
+h_e_kohler=schemeswitch('k',c,x);
+h_e_colinet=schemeswitch('c',c,x);
+fprintf('The value using Muggianu Scheme : %d J/mol.\n',h_e_muggianu);
+fprintf('The value using Kohler Scheme : %d J/mol.\n',h_e_kohler);
+fprintf('The value using Colinet Scheme : %d J/mol.\n',h_e_colinet);
+
+function h_E = schemeswitch(scheme,c,x)
+% getting scheme related values
+switch scheme
+    case 'm' % Muggianu
+        t_ij=1;
+        beta_ij=1;
+    case 'k' % Kohler
+        t_ij=1;
+        beta_ij=1;
+    case 'c' % Colinet
+        t_ij=2;
+        beta_ij=0.5; % since it's the same for both
+end
+h_E=zeros(size(x,1),1);
 % The general schema :
 for ii=1:size(x,1)
     for i=1:c-1
         for j=i+1:c
             for k=1:t_ij
-                % disp(num2str(i));
-                % disp(num2str(j));
-                
-                % scheme related values
+                % lambda also depends on scheme
                 % % muggianu
+                switch scheme
+                    case 'm' % Muggianu
+                        lambda_ij=0;
+                        lambda_ji=0;
+                    case 'k' % Kohler
+                        lambda_ij=(x(ii,i)-x(ii,j))/(x(ii,i)+x(ii,j));
+                        lambda_ji=(x(ii,j)-x(ii,i))/(x(ii,i)+x(ii,j));
+                    case 'c' % Colinet
+                        if(t_ij==1)
+                            lambda_ij=1;
+                            lambda_ji=1;
+                        else
+                            lambda_ij=-1;
+                            lambda_ji=-1;
+                        end
+                end
                 
-                lambda_ij=0;
+                [p,q,r]=parameters(x(ii,i),x(ii,j),lambda_ij,lambda_ji);
                 
-                % % Kohler
-                
-                % lambda_ij=(x(ii,i)-x(ii,j))/(x(ii,i)+x(ii,j));
-                
-                % % Colinet
-    
-%                 if(t_ij==1)
-%                     lambda_ij=1;
-%                 else
-%                     lambda_ij=-1;
-%                 end
-                
-                % disp(num2str(lambda_ij)); 
-                [p,q,r]=parameters(x(ii,i),x(ii,j),lambda_ij);
-                % disp(num2str([p,q]));
                 if(i==1)
                     if(j==2)
                         gE=fCoCu(p,q);
@@ -79,10 +103,11 @@ for ii=1:size(x,1)
         end
     end
 end
+end
 
-function [x_ij,x_ji,f_ij]=parameters(x_1,x_2,lambda_ij)
+function [x_ij,x_ji,f_ij]=parameters(x_1,x_2,lambda_ij,lambda_ji)
 x_ij=0.5*((1+x_1-x_2)+lambda_ij*(1-x_1-x_2));
-x_ji=0.5*((1+x_2-x_1)+lambda_ij*(1-x_1-x_2));
+x_ji=0.5*((1+x_2-x_1)+lambda_ji*(1-x_1-x_2));
 f_ij=(x_1*x_2)/(x_ij*x_ji);
 end
 
@@ -99,7 +124,7 @@ h = x_1*x_2*(1331);
 end
 
 function h = fCuFe(x_1,x_2)
-h = x_1*x_2*(35626 - 1530*(x_1-x_2) + 12714*(x_1-x_2)^2 - 1177*(x_1-x_2)^3);
+h = x_1*x_2*(35626 - 1530*(x_1-x_2) + 12714*(x_1-x_2)^2 + 1177*(x_1-x_2)^3);
 end
 
 function h = fCuNi(x_1,x_2)
@@ -107,5 +132,5 @@ h = x_1*x_2*(12049 - 1862*(x_1-x_2));
 end
 
 function h = fFeNi(x_1,x_2)
-h = x_1*x_2*(-18379 - 9228*(x_1-x_2));
+h = x_1*x_2*(-18379 + 9228*(x_1-x_2));
 end
